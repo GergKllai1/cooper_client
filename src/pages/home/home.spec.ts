dc3ec06190d3b84aea3e685fed9c0e94823a1dc6
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-12 10:32:54 +0100

    Debugs unit tests, tests are going green

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -1,11 +1,11 @@
-import { HomePage } from './home';
+import { HomePage } from "./home";
 import { TestBed, async } from "@angular/core/testing";
 import { IonicModule, Platform, NavController } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
 
-describe('Homepage', () => {
+describe("HomePage", () => {
     let fixture, homepage;
 
     beforeEach(async(() => { 
@@ -15,10 +15,10 @@ describe('Homepage', () => {
             ],
             imports: [IonicModule.forRoot(HomePage)],
             providers: [
-                { provide: Platform, userFactory: () => PlatformMock.instance() },
-                { provide: StatusBar, userFactory: () => StatusBarMock.instance() },
-                { provide: SplashScreen, userFactory: () => SplashScreenMock.instance() },
-                { provide: NavController, userFactory: () => NavControllerMock.instance() },
+                { provide: Platform, useFactory: () => PlatformMock.instance() },
+                { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
+                { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
+                { provide: NavController, useFactory: () => NavControllerMock.instance() },
             ]
         }).compileComponents();
     }));
@@ -44,4 +44,5 @@ describe('Homepage', () => {
 
         expect(homepage.calculate).toHaveBeenCalled();
     });
+
 });
