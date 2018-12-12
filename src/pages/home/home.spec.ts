37fbbbba659f2286c273eaa665a03b75036a72bc
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-12 13:27:01 +0100

    Adds calculate method to display result in console, adds providers to home.spec

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -1,9 +1,11 @@
 import { HomePage } from "./home";
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, inject } from "@angular/core/testing";
 import { IonicModule, Platform, NavController } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
+import { PersonProvider } from './../../providers/person/person';
+import { CooperProvider } from './../../providers/cooper/cooper';
 
 describe("HomePage", () => {
     let fixture, homepage;
@@ -19,6 +21,8 @@ describe("HomePage", () => {
                 { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
                 { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
                 { provide: NavController, useFactory: () => NavControllerMock.instance() },
+                PersonProvider,
+                CooperProvider
             ]
         }).compileComponents();
     }));
@@ -37,12 +41,27 @@ describe("HomePage", () => {
         expect(homepage.user).toEqual({ distance: 1000, age: 20});
     });
 
-    it('should have calculate funciton', () => {
+    it('should have calculate function', () => {
         spyOn(homepage, 'calculate');
 
         homepage.calculate();
 
         expect(homepage.calculate).toHaveBeenCalled();
     });
+
+    it('calculate function should call person provider doAssesement function', inject(
+        [PersonProvider],
+        person => {
+            homepage.user = { age: 25, gender: 'female', distance: 2500};
+            spyOn(person, 'doAssessment').and.returnValue('Above average');
+
+            homepage.calculate();
+
+            expect(person.doAssessment).toHaveBeenCalled();
+            expect(person.doAssessment).toHaveBeenCalledWith(2500);
+            expect(person.age).toEqual(25);
+            expect(person.gender).toEqual("female");
+        }
+    ));
 
 });
