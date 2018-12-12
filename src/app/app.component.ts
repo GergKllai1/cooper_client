4a9fc6ffccdae23f192c7d7ff83902c9faf09b67
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-12 16:02:46 +0100

    Adds login logout unit tests to app spec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Angular2TokenService } from 'angular2-token';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,7 +16,14 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, 
+              public statusBar: StatusBar, 
+              public splashScreen: SplashScreen,
+              private _tokenService: Angular2TokenService) {
+    this._tokenService.init({
+      apiBase: 'https://gk-cooper-api.herokuapp.com/api/v1'
+    })
+    
     this.initializeApp();
     this.pages = [
       { title: 'Home', component: HomePage },
